357ecc84247220f9398257dfcbb3f48d16065117
Camilo <caferrerb@gmail.com>
2017-12-19 08:53:12 -0500

    pruebas rest

---
+++ src/test/groovy/com/velocity/caferrer/test/cases/ApiSpec.groovy
@@ -0,0 +1,71 @@
+package com.velocity.caferrer.test.cases
+
+import org.junit.experimental.categories.Category
+import spock.lang.Shared
+import spock.lang.Specification
+import groovyx.net.http.RESTClient
+
+@Category(util.UnitTest)
+class ApiSpec extends  Specification{
+
+    @Shared
+    def client
+
+
+    def setupSpec() {
+        client = new RESTClient('http://localhost:8080/persona/')
+    }
+
+    def "test create person"(){
+
+        setup:
+            println "setup"
+
+
+        when:
+            println "when"
+            def resp = client.post(path:"crear", body:[nombre:nombre, apellido:apellido,cedula:cedula,edad:edad],
+                requestContentType:'application/json')
+
+            def respAssert = client.get(path: "buscar/${cedula}")
+
+
+        then:
+            println "then"
+
+        with (resp) {
+            status == 200
+            data.codigo == '00'
+            data.mensaje == 'OK'
+
+        }
+
+        with (respAssert) {
+                status == 200
+                data.codigo == '00'
+                data.mensaje == 'OK'
+                data.obj.nombre == nombre
+                data.obj.cedula == cedula
+                data.obj.apellido == apellido
+                (data.obj.edad+"")==edad
+            }
+
+
+        where:
+            nombre<<['claudia','camilo']
+            apellido<<['fernandez','ferrer']
+            cedula<<['1234','4321']
+            edad<<['27','34']
+
+
+    }
+
+    def cleanup(){
+       println("limpiando test....")
+        client.get(path:'deleteall' )
+
+    }
+
+
+
+}
---
+++ src/test/groovy/util/UnitTest.groovy
@@ -0,0 +1,8 @@
+package util
+
+/**
+ * This is a marker interface that identifies all Groovy specification
+ * classes which contain unit tests.
+ */
+interface UnitTest {
+}
