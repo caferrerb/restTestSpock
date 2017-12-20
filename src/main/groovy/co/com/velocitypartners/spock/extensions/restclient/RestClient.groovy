cfd914aaa0f1c8b4d7994971770a81536e12fd08
Camilo <caferrerb@gmail.com>
2017-12-20 14:26:28 -0500

    pruebas rest

--- src/main/groovy/co/com/velocitypartners/spock/extensions/restclient/RestClient.groovy
+++ src/main/groovy/co/com/velocitypartners/spock/extensions/restclient/RestClient.groovy
@@ -1,16 +1,16 @@
-package co.com.velocitypartners.spock.extensions.restclient;
+package co.com.velocitypartners.spock.extensions.restclient
 
-import org.spockframework.runtime.extension.ExtensionAnnotation;
+import org.spockframework.runtime.extension.ExtensionAnnotation
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.ElementType
+import java.lang.annotation.Retention
+import java.lang.annotation.RetentionPolicy
+import java.lang.annotation.Target
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
-@ExtensionAnnotation(RestClientExtension.class)
- @interface RestClient {
+@ExtensionAnnotation(RestClientExtension)
+@interface RestClient {
 
     boolean hardCoded() default true
     String baseUrl()
