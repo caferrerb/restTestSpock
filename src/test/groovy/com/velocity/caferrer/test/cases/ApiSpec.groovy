c82aff3788dd040f6e3494e81f323aca38ab8fd0
Camilo <caferrerb@gmail.com>
2017-12-20 09:45:18 -0500

    pruebas rest

--- src/test/groovy/com/velocity/caferrer/test/cases/ApiSpec.groovy
+++ src/test/groovy/com/velocity/caferrer/test/cases/ApiSpec.groovy
@@ -60,10 +60,118 @@ class ApiSpec extends  Specification{
 
     }
 
+
+    def "list persons"(){
+
+       setup:
+            def respsizeExpect=client.get(path:"listar")
+            def sizeExpect=respsizeExpect.data.obj.size
+
+       when:
+            def respCrear = client.post(path:"crear", body:[nombre:'camilo', apellido:'ferrer',cedula:'12341234',edad:12],
+                requestContentType:'application/json')
+
+            def respsizeReal=client.get(path:"listar")
+            def sizeReal=respsizeReal.data.obj.size
+            
+
+
+       then:
+            respCrear.status==200
+            respsizeReal.status==200
+
+            sizeReal==sizeExpect+1
+    }
+
+
+    def "edit person"(){
+        setup:
+            def respCrear = client.post(path:"crear", body:[nombre:nombre, apellido:apellido,cedula:cedula,edad:edad],
+                requestContentType:'application/json')
+        when:
+
+            def respEditar =client.put(path:"editar", body:[nombre:nombreeditar, apellido:apellidoeditar,cedula:cedula,edad:edadeditar],
+                    requestContentType:'application/json')
+
+            def respBuscar = client.get(path: "buscar/${cedula}")
+
+        then:
+
+            respCrear.status==200
+            respEditar.status==200
+
+            with (respBuscar) {
+                status == 200
+                data.codigo == '00'
+                data.mensaje == 'OK'
+                data.obj.nombre==nombreeditar
+                data.obj.apellido==apellidoeditar
+                data.obj.edad==Integer.valueOf(edadeditar)
+            }
+
+
+
+        where:
+
+            nombre<<['claudia','camilo']
+            apellido<<['fernandez','ferrer']
+            cedula<<['1234','4321']
+            edad<<['27','34']
+
+            nombreeditar<<['claudiaeditado','camiloeditado']
+            apellidoeditar<<['fernandezeditado','ferrereditado']
+            edadeditar<<['34','28']
+
+    }
+
+
+
+    def "find person"(){
+
+        setup:
+            def respCrear = client.post(path:"crear", body:[nombre:nombre, apellido:apellido,cedula:cedula,edad:edad],
+                    requestContentType:'application/json')
+
+        when:
+            def respBuscar = client.get(path: "buscar/${cedula}")
+
+        then:
+
+            with (respCrear) {
+                status == 200
+                data.codigo == '00'
+                data.mensaje == 'OK'
+
+            }
+
+            with (respBuscar) {
+                status == 200
+                data.codigo == '00'
+                data.mensaje == 'OK'
+                data.obj.nombre==nombre
+                data.obj.apellido==apellido
+                data.obj.edad==Integer.valueOf(edad)
+            }
+
+        where:
+            nombre<<['claudia','camilo']
+            apellido<<['fernandez','ferrer']
+            cedula<<['1234','4321']
+            edad<<['27','34']
+
+
+    }
+
     def cleanup(){
        println("limpiando test....")
         client.get(path:'deleteall' )
 
+
+    }
+
+
+    def cleanupSpec(){
+        client.shutdown()
     }
 
 
